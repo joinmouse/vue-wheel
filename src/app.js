c0e683df3317598a86e6166f6b6fd1fc31a40d2d
joinmouse <908380434@qq.com>
2018-11-08 23:33:56 +0800

    改写为Vue单文件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('ui-button', Button)
+
+new Vue({
+    el: '#app',
+})
---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="ui-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: ''
+    }
+</script>
+
+<style less="scss">
+.ui-button {
+    height: var(--button-height);
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    &:hover{
+        border-color: var(--border-color-hover)
+    }
+    &:active {
+        background: var(--button-active-bg)
+    }
+    &:focus {
+        outline: none;
+    }
+}
+</style>
