0cae61b4993dfe2ba4a5995a62acfdf3a475aa25
joinmouse <908380434@qq.com>
2018-11-10 10:06:36 +0800

    验证iconPosition为left或者right

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,16 @@
 
 <script>
     export default {
-        props: ['icon', 'iconPosition']  //left, right
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                   return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 
