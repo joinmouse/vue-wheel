e1c1cc08168be90df41ea81f888ac3f052356ed9
joinmouse <908380434@qq.com>
2018-11-11 18:16:02 +0800

    创建button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
+import ButtonGroup from './button-group';
 
 Vue.component('ui-button', Button)
 Vue.component('ui-icon', Icon)
+Vue.component('ui-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ui-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+    .ui-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .ui-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-buttom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
