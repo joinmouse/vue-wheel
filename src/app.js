f1cc1b66d1fa347b155e1cb556ee31b078df339d
joinmouse <908380434@qq.com>
2018-11-14 23:21:07 +0800

    写input基本样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
+import Input from './input';
 
 Vue.component('ui-button', Button)
 Vue.component('ui-icon', Icon)
 Vue.component('ui-button-group', ButtonGroup)
+Vue.component('ui-input', Input)
 
 new Vue({
     el: '#app',
@@ -13,73 +15,3 @@ new Vue({
         loading1: false
     }
 })
-
-// 单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-const expect = chai.expect
-chai.use(spies)
-
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
-}
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    console.log(order)
-    expect(order).to.eq("2")
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    // mock
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    vm.$mount()
-    let spy = chai.spy(function(){})
-    vm.$on('click', spy)
-    // 希望这个函数被执行
-    vm.$el.click()
-    expect(spy).to.have.been.called()
-}
-
-
-
-
-
