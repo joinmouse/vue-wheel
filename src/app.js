e0f4de01252ceca24420cafc1aa054c109d43156
joinmouse <908380434@qq.com>
2018-12-03 01:03:31 +0800

    添加toast和plugin

--- src/app.js
+++ src/app.js
@@ -3,17 +3,24 @@ import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
 import Input from './input';
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('ui-button', Button)
 Vue.component('ui-icon', Icon)
 Vue.component('ui-button-group', ButtonGroup)
 Vue.component('ui-input', Input)
+Vue.component('ui-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
     el: '#app',
     data: {
         loading1: false,
         message: '王五'
+    },
+    created() {
+        this.$toast('测试一下 toast')
     },
     methods: {
         inputChange(e) {
---
+++ src/toast.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UIToast',
+    props: {
+        autoClose: {
+            type: Boolean,
+            default: true
+        },
+        autoCloseDelay: {
+            type: Number,
+            default: 5
+        }
+    },
+    mounted() {
+        if(this.autoClose) {
+            setTimeout(() => {
+                this.close()
+            }, this.autoCloseDelay * 1000)
+        }
+    },
+    methods: {
+        close() {
+            this.$el.remove()
+            this.$destroy()
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0,0,0,0.75);
+
+.toast {
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 4px;
+    box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+    height: $toast-height;
+    font-size: 14px;
+    line-height: 1.8;
+    background: $toast-bg;
+    color: white;
+}
+</style>
